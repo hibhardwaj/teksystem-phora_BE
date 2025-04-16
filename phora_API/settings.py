d2fa41d862bd6413f297598e2d2880ecc84bd0f3
Himani Bhardwaj <hibhardwaj@teksystems.com>
2025-04-16 18:51:54 +0530

    added few steps in the configuration

--- phora_API/settings.py
+++ phora_API/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-CSRF_TRUSTED_ORIGINS=['https://'+config('ALLOWED_HOSTS')]
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# CSRF_TRUSTED_ORIGINS=['https://'+config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.azurewebsites.net']
+CORS_ALLOW_ALL_ORIGINS: True
 # Application definition
 
 INSTALLED_APPS = [
