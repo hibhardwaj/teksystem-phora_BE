685a39702a8f74a52c854299b8d1487f324e0427
Himani Bhardwaj <hibhardwaj@teksystems.com>
2025-04-17 20:40:47 +0530

    API calls integrated

--- phora_API/urls.py
+++ phora_API/urls.py
@@ -20,6 +20,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.views.generic import RedirectView
 from django.conf import settings
+from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
